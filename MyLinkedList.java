6c138fec073c528a224ffce5e6f384067fe76090
jason zhang <jzhang20@stuy.edu>
2021-01-09 13:17:14 -0500

    fixed my code part 3

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -12,19 +12,21 @@ public class MyLinkedList{
 
   public boolean add (String value) {
     Node newFinal = new Node (value);
-    MyLinkedList linky = new MyLinkedList ();
+    System.out.println(value);
+    System.out.println(size());
     if (size() > 0) {
       end.setNext(newFinal);
       newFinal.setPrev(end);
       end = newFinal;
       size += 1;
-      return true;
+    } else {
+      newFinal.setPrev(null);
+      newFinal.setNext(null);
+      end = newFinal;
+      start = newFinal;
+      size += 1;
     }
-    newFinal.setPrev(null);
-    newFinal.setNext(null);
-    end = newFinal;
-    start = newFinal;
-    size += 1;
+    System.out.println(size());
     return true;
   }
 
@@ -32,47 +34,53 @@ public class MyLinkedList{
     Node newNode = new Node (value);
     Node next = start;
     int counter = 0;
-    if (index == size()) {
-      add(value);
+    System.out.println(value);
+    System.out.println(size());
+    if (index < 0 || index > size()) {
+      throw new IndexOutOfBoundsException ("no");
     } else {
-      if (index == 0) {
-        start.setPrev(newNode);
-        newNode.setNext(start);
-        newNode.setPrev(null);
-        start = newNode;
-        size += 1;
+      if (index == size()) {
+        add(value);
       } else {
-        for (int i = 0; i < size() - 1; i++) {
-          if (counter == index) {
-            next = next.getPrev();
-            newNode.setPrev(next);
-            newNode.setNext(next.getNext());
-            next.setNext(newNode);
-            next = newNode.getNext();
-            next.setPrev(newNode);
-            i = size();
-          } else {
-            next = next.getNext();
-            counter += 1;
+        if (index == 0) {
+          start.setPrev(newNode);
+          newNode.setNext(start);
+          newNode.setPrev(null);
+          start = newNode;
+          size += 1;
+        } else {
+          for (int i = 0; i < size(); i++) {
+            if (counter == index) {
+              next = next.getPrev();
+              newNode.setPrev(next);
+              newNode.setNext(next.getNext());
+              next.setNext(newNode);
+              next = newNode.getNext();
+              next.setPrev(newNode);
+              i = size();
+            } else {
+              next = next.getNext();
+              counter += 1;
+            }
           }
+          size += 1;
         }
-        size += 1;
       }
     }
+    System.out.println(size());
   }
 
   public String get (int index) {
-    int counter = 0;
     Node nodeTrack = start;
     if (index >= size()) {
       throw new IndexOutOfBoundsException ("no");
     }
     for (int i = 0; i < size(); i++) {
-      if (index != counter) {
-        nodeTrack = nodeTrack.getNext();
-        counter += 1;
-      } else {
+      if (index == i) {
         i += size();
+        System.out.println(nodeTrack.getData());
+      } else {
+        nodeTrack = nodeTrack.getNext();
       }
     }
     return nodeTrack.getData();
@@ -96,18 +104,39 @@ public class MyLinkedList{
   }
 
   public String toString() {
-    Node nodeTrack = start;
     String outputStuff = "[";
-    for (int i = 0; i < size(); i++) {
-      outputStuff += nodeTrack.getData();
-      nodeTrack = nodeTrack.getNext();
-      outputStuff += ", ";
-    }
     if (size() == 0) {
+      return "[]";
+    }
+    for (int i = 0; i < size(); i++) {
+      outputStuff += get(i);
+      outputStuff += ", ";
+      System.out.println(outputStuff);
+    }
+    if (outputStuff.length() < 2) {
       return "[]";
     }
     outputStuff = outputStuff.substring(0, outputStuff.length() - 2);
     outputStuff += "]";
     return outputStuff;
   }
+
+  public String toStringReversed() {
+    String outputStuff = "[";
+    if (size() == 0) {
+      return "[]";
+    }
+    for (int i = size() - 1; i >= 0; i--) {
+      outputStuff += get(i);
+      outputStuff += ", ";
+      System.out.println(outputStuff);
+    }
+    if (outputStuff.length() < 2) {
+      return "[]";
+    }
+    outputStuff = outputStuff.substring(0, outputStuff.length() - 2);
+    outputStuff += "]";
+    return outputStuff;
+  }
+
 }
